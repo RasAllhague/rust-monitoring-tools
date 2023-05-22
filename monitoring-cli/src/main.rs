abbafc5ced3eecd8dadd3750d4753a0e7b79fb06
RasAllhague <50292283+RasAllhague@users.noreply.github.com>
2023-05-22 15:12:51 +0200

    implemented models for system information gathering

--- monitoring-cli/src/main.rs
+++ monitoring-cli/src/main.rs
@@ -1,6 +1,16 @@
+mod config;
+
+use monitoring_core::models::{SystemInformation};
+use systemstat::{Platform, System};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
+    let system = System::new();
+    let hostname = hostname::get()?;
+    let os_info = os_info::get();
+
+    let system_info = SystemInformation::collect(system, hostname, os_info)?;
+    
     
 
     Ok(())
--- monitoring-core/src/models.rs
+++ monitoring-core/src/models.rs
@@ -1,10 +1,14 @@
-use std::{time::Duration, net::{Ipv4Addr, Ipv6Addr}, collections::HashMap};
+use std::{time::Duration, net::{Ipv4Addr, Ipv6Addr}, collections::HashMap, ffi::OsString, thread::sleep};
 
-use chrono::NaiveDateTime;
+use chrono::{NaiveDateTime, NaiveDate};
+use os_info::Info;
 use serde::{Serialize, Deserialize};
+use systemstat::{System, Platform, CPULoad};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SystemInformation {
+    pub hostname: OsString,
+    pub os_info: Info,
     pub cpu: CpuInformation,
     pub load_avg: LoadAverage,
     pub memory: Memory,
@@ -12,20 +16,20 @@ pub struct SystemInformation {
     pub battery_life: Option<BatteryLife>,
     pub mounts: Vec<Filesystem>,
     pub networks: HashMap<String, Network>,
-    pub net_stats: NetworkStatistics,
+    pub net_stats: Vec<NetworkStatistics>,
     pub socket_stats: SocketStatistics,
     pub uptime: Duration,
     pub boot_time: NaiveDateTime,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CpuInformation {
     pub temperature: f32,
     pub loads: Vec<CpuLoad>,
     pub aggregate_load: CpuLoad,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CpuLoad {
     pub user: f32,
     pub nice: f32,
@@ -34,32 +38,32 @@ pub struct CpuLoad {
     pub idle: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct LoadAverage {
     pub one: f32,
     pub five: f32,
     pub fifteen: f32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Memory {
     total: u64,
     free: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Swap {
     total: u64,
     free: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct BatteryLife {
     pub remaining_capacity: f32,
     pub remaining_time: Duration,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Filesystem {
     pub files: usize,
     pub files_total: usize,
@@ -73,7 +77,7 @@ pub struct Filesystem {
     pub fs_mounted_on: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum IpAddress {
     Empty,
     Unsupported,
@@ -81,19 +85,19 @@ pub enum IpAddress {
     V6(Ipv6Addr),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct NetworkAddress {
     pub addr: IpAddress,
     pub netmask: IpAddress,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Network {
     pub name: String,
     pub addrs: Vec<NetworkAddress>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct NetworkStatistics {
     pub rx_bytes: u64,
     pub tx_bytes: u64,
@@ -103,11 +107,208 @@ pub struct NetworkStatistics {
     pub tx_errors: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SocketStatistics {
     pub tcp_sockets_in_use: usize,
     pub tcp_sockets_orphaned: usize,
     pub udp_sockets_in_use: usize,
     pub tcp6_sockets_in_use: usize,
     pub udp6_sockets_in_use: usize,
+}
+
+impl SystemInformation {
+    pub fn collect(system: System, hostname: OsString, os_info: Info) -> anyhow::Result<Self> {
+        let mut mounts = Vec::new();
+        
+        for fs in system.mounts()? {
+            mounts.push(Filesystem::from(fs));
+        };
+
+        let mut networks = HashMap::new();
+
+        for (key, value) in system.networks()? {
+            networks.insert(key, Network::from(value));
+        }
+
+        let mut net_stats = Vec::new();
+
+        for (network, _) in system.networks()? {
+            net_stats.push(NetworkStatistics::from(system.network_stats(&network)?));
+        }
+    
+        let boot_time = { 
+            let boot_time = system.boot_time()?;
+            NaiveDate::from_ymd_opt(boot_time.year(), boot_time.month() as u32, boot_time.day() as u32)
+                .unwrap()
+                .and_hms_opt(boot_time.hour() as u32, boot_time.minute() as u32, boot_time.second() as u32)
+                .unwrap()
+        };
+
+        let battery_life = match system.battery_life() {
+            Ok(b) => Some(BatteryLife::from(b)),
+            Err(_) => None,
+        };
+
+        Ok(Self {
+            hostname,
+            os_info,
+            cpu: CpuInformation::collect(&system)?,
+            load_avg: LoadAverage::from(system.load_average()?),
+            memory: Memory::from(system.memory()?),
+            swap: Swap::from(system.swap()?),
+            battery_life: battery_life,
+            mounts,
+            networks,
+            net_stats,
+            socket_stats: SocketStatistics::from(system.socket_stats()?),
+            uptime: system.uptime()?,
+            boot_time: boot_time,
+        })
+    }
+}
+
+impl CpuInformation {
+    pub fn collect(system: &System) -> anyhow::Result<Self> {
+        let temp = system.cpu_temp()?;
+        let (cpu_loads, load_aggregate) = { 
+            let load = system.cpu_load()?;
+            let aggregate = system.cpu_load_aggregate()?;
+            
+            sleep(Duration::from_secs(1));
+
+            (load.done()?, aggregate.done()?)
+        }; 
+
+        let mut loads = Vec::new();
+
+        for load in cpu_loads {
+            loads.push(CpuLoad::from(load));
+        }
+
+        Ok(Self {
+            temperature: temp,
+            loads: loads,
+            aggregate_load: CpuLoad::from(load_aggregate),
+        })
+    }
+}
+
+impl From<CPULoad> for CpuLoad {
+    fn from(value: CPULoad) -> Self {
+        Self { user: value.user, nice: value.nice, system: value.system, interrupt: value.interrupt, idle: value.idle }
+    }
+}
+
+impl From<systemstat::LoadAverage> for LoadAverage {
+    fn from(value: systemstat::LoadAverage) -> Self {
+        Self {
+            one: value.one,
+            five: value.five,
+            fifteen: value.fifteen,
+        }
+    }
+}
+
+impl From<systemstat::Memory> for Memory {
+    fn from(value: systemstat::Memory) -> Self {
+        Self {
+            total: value.total.0,
+            free: value.free.0,
+        }
+    }
+}
+
+impl From<systemstat::Swap> for Swap {
+    fn from(value: systemstat::Swap) -> Self {
+        Self {
+            total: value.total.0,
+            free: value.free.0,
+        }
+    }
+}
+
+impl From<systemstat::BatteryLife> for BatteryLife {
+    fn from(value: systemstat::BatteryLife) -> Self {
+        Self {
+            remaining_capacity: value.remaining_capacity,
+            remaining_time: value.remaining_time,
+        }
+    }
+}
+
+impl From<systemstat::Filesystem> for Filesystem {
+    fn from(value: systemstat::Filesystem) -> Self {
+        Self {
+            files: value.files,
+            files_total: value.files_total,
+            files_avail: value.files_avail,
+            free: value.free.0,
+            avail: value.avail.0,
+            total: value.total.0,
+            name_max: value.name_max,
+            fs_type: value.fs_type,
+            fs_mounted_from: value.fs_mounted_from,
+            fs_mounted_on: value.fs_mounted_on,
+        }
+    }
+}
+
+impl From<systemstat::Network> for Network {
+    fn from(value: systemstat::Network) -> Self {
+        let mut addrs = Vec::new();
+
+        for addr in value.addrs {
+            addrs.push(NetworkAddress::from(addr));
+        }
+
+        Self {
+            name: value.name,
+            addrs,
+        }
+    }
+}
+
+impl From<systemstat::NetworkAddrs> for NetworkAddress {
+    fn from(value: systemstat::NetworkAddrs) -> Self {
+        Self {
+            addr: IpAddress::from(value.addr),
+            netmask: IpAddress::from(value.netmask),
+        }
+    }
+}
+
+impl From<systemstat::IpAddr> for IpAddress {
+    fn from(value: systemstat::IpAddr) -> Self {
+        match value {
+            systemstat::IpAddr::Empty => IpAddress::Empty,
+            systemstat::IpAddr::Unsupported => IpAddress::Unsupported,
+            systemstat::IpAddr::V4(v4) => IpAddress::V4(v4),
+            systemstat::IpAddr::V6(v6) => IpAddress::V6(v6),
+        }
+    }
+}
+
+impl From<systemstat::NetworkStats> for NetworkStatistics {
+    fn from(value: systemstat::NetworkStats) -> Self {
+        Self {
+            rx_bytes: value.rx_bytes.0,
+            tx_bytes: value.tx_bytes.0,
+            rx_packets: value.rx_packets,
+            tx_packets: value.tx_packets,
+            rx_errors: value.rx_errors,
+            tx_errors: value.tx_errors,
+        }
+    }
+}
+
+impl From<systemstat::SocketStats> for SocketStatistics {
+    fn from(value: systemstat::SocketStats) -> Self {
+        Self {
+            tcp6_sockets_in_use: value.tcp6_sockets_in_use,
+            tcp_sockets_in_use: value.tcp_sockets_in_use,
+            udp6_sockets_in_use: value.udp6_sockets_in_use,
+            udp_sockets_in_use: value.udp_sockets_in_use,
+            tcp_sockets_orphaned: value.tcp_sockets_orphaned,
+        }
+    }
 }
